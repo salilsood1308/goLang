a3d6cb06907d60c83c93be615ed2943c76a7ae07
Salil <salil.sood@gmail.com>
2021-10-04 19:37:04 -0400

    Updated calling functions

--- practise01/mymain/go.mod
+++ practise01/mymain/go.mod
@@ -2,7 +2,7 @@ module github.com/salilsood1308/goLang/practise01/mymain
 
 go 1.17
 
-require github.com/salilsood1308/goLang/practise01/myutils v0.0.0-20211002015311-de713abb577d
+require github.com/salilsood1308/goLang/practise01/myutils v0.0.0-20211004202219-40e48ef6f810
 
 require (
 	golang.org/x/text v0.3.7 // indirect
--- practise01/mymain/hello.go
+++ practise01/mymain/hello.go
@@ -9,7 +9,20 @@ import (
 
 func main() {
 	log.SetPrefix("greetings: ")
-	fmt.Println(myutils.Greet1("Salil"))
+	// log.SetFlags(0)
+
 	fmt.Println(myutils.Greet2())
 	fmt.Println(myutils.Myquote())
+
+	message, err := myutils.Greet1("Ishan")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
+	mesg, err := myutils.Greet1("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(mesg)
 }
