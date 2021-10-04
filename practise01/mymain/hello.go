40e48ef6f810e6d6abd017494931233c2d8c3cb4
Salil <salil.sood@gmail.com>
2021-10-04 16:22:19 -0400

    Added Error Handling

--- practise01/mymain/hello.go
+++ practise01/mymain/hello.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	myutils "github.com/salilsood1308/goLang/practise01/myutils"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
 	fmt.Println(myutils.Greet1("Salil"))
 	fmt.Println(myutils.Greet2())
 	fmt.Println(myutils.Myquote())
--- practise01/myutils/greetingUtils.go
+++ practise01/myutils/greetingUtils.go
@@ -1,17 +1,31 @@
 package myutils
 
 import (
+	"errors"
 	"fmt"
 
 	"rsc.io/quote"
 )
 
-func Greet1(name string) string {
-	return fmt.Sprintf("Hello %v", name)
+func Greet1(name string) (string, error) {
+	// if name == "" {
+	// 	return "", errors.New("Empty name")
+	// }
+	// message := fmt.Sprintf("Hello! %v", name)
+	// return message, nil
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
 
 func Greet2() string {
-	return ("Hello World! Courtsey SS-Modules")
+	return "Hello World! Courtsey SS-Modules"
 }
 
 func Myquote() string {
